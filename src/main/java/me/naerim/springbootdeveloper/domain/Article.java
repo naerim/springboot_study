c3e8d16c7f0667fb1009857b372959961f79cc91
naerim <naerim1119@gmail.com>
2024-07-30 11:57:11 +0900

    setting: project setting

---
+++ src/main/java/me/naerim/springbootdeveloper/domain/Article.java
@@ -0,0 +1,29 @@
+package me.naerim.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+    @Id // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동으로 1씩 증가
+    @Column(name="id", updatable = false) // not null
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
