a76e605f75deb4bd67e89da0d00544a5dd7ce854
naerim <naerim1119@gmail.com>
2024-07-30 17:20:29 +0900

    feat: 블로그 글 작성 service 및 controller 생성

---
+++ src/main/java/me/naerim/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,23 @@
+package me.naerim.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.naerim.springbootdeveloper.domain.Article;
+import me.naerim.springbootdeveloper.dto.AddArticleRequest;
+import me.naerim.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+}
---
+++ src/main/java/me/naerim/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,17 @@
+package me.naerim.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.naerim.springbootdeveloper.domain.Article;
+import me.naerim.springbootdeveloper.dto.AddArticleRequest;
+import me.naerim.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service // 빈으로 등록
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) { // 블로그 글 추가
+        return blogRepository.save(request.toEntity());
+    }
+}
