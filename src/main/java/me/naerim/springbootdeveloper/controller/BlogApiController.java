39fc4567fbd8e3aeb7bc5419bb2909e93b15c00b
naerim <naerim1119@gmail.com>
2024-07-31 13:56:30 +0900

    feat: 블로그 글 조회

--- src/main/java/me/naerim/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/naerim/springbootdeveloper/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import me.naerim.springbootdeveloper.dto.ArticleResponse;
 import me.naerim.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,11 @@ public class BlogApiController {
     public ResponseEntity<List<ArticleResponse>> findAllArticles() {
         List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).toList();
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+        return ResponseEntity.ok().body(new ArticleResponse(article));
     }
 }
