e617ad97152c5c3152c899446b6e8a8680756012
naerim <naerim1119@gmail.com>
2024-08-02 12:36:25 +0900

    feat: 블로그 글 생성/수정/삭제 뷰

--- src/main/java/me/naerim/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/naerim/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,15 @@
 package me.naerim.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.naerim.springbootdeveloper.domain.Article;
 import me.naerim.springbootdeveloper.dto.ArticleListViewResponse;
+import me.naerim.springbootdeveloper.dto.ArticleViewResponse;
 import me.naerim.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,5 +23,23 @@ public class BlogViewController {
         List<ArticleListViewResponse> articles = blogService.findAll().stream().map(ArticleListViewResponse::new).toList();
         model.addAttribute("articles", articles);
         return "articleList"; // articleList.html 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if(id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else { // id가 없으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
