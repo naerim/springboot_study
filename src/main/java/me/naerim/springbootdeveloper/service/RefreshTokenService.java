82a05c82011ef9aac1be50eb2d1ad0d027ca9541
naerim <naerim1119@gmail.com>
2024-08-10 22:09:09 +0900

    feat: 리프레시 토큰 서비스 및 컨트롤러

---
+++ src/main/java/me/naerim/springbootdeveloper/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package me.naerim.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.naerim.springbootdeveloper.domain.RefreshToken;
+import me.naerim.springbootdeveloper.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("unexpected token"));
+    }
+}
